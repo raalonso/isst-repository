01e4e113438185eb382e75211e4608bcf6ea686b
Doris Sângeap <dorys.ana@gmail.com>
2015-04-17 10:30:25 +0200

    Lista de alarmas atendidas por cada usuario

--- teleassistence-app/src/es/isst/ca/InfoUsuarioServlet.java
+++ teleassistence-app/src/es/isst/ca/InfoUsuarioServlet.java
@@ -1,6 +1,10 @@
 package es.isst.ca;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,6 +15,9 @@ import javax.servlet.http.HttpServletResponse;
 import es.isst.ca.dao.UserDAO;
 import es.isst.ca.dao.UserDAOImpl;
 import es.isst.ca.model.Usuario;
+import es.isst.ca.dao.AlarmDAO;
+import es.isst.ca.dao.AlarmDAOImpl;
+import es.isst.ca.model.Alarm;
 
 public class InfoUsuarioServlet extends HttpServlet {
 
@@ -21,9 +28,26 @@ public class InfoUsuarioServlet extends HttpServlet {
 		String id = req.getParameter("id");
 
 		UserDAO userdao = UserDAOImpl.getInstance();
+		AlarmDAO alarmdao = AlarmDAOImpl.getInstance();
 			
 		Usuario usuario = userdao.getUsuarioById(Long.parseLong(id));
+		List<Alarm> alarms = new ArrayList<Alarm>();
 		
+		alarms = alarmdao.listAttendedAlarms(usuario.getIMEI());
+		Comparator<Alarm> comparador = new Comparator<Alarm>() {
+			public int compare(Alarm a, Alarm b) {
+				int resultado = (int) Long.compare(a.getTimestamp(), b.getTimestamp());
+				if (resultado != 0) {
+					return resultado;
+				} else {
+					return -1;
+				}
+			}
+		};
+		
+		Collections.sort(alarms, comparador);
+		
+		req.getSession().setAttribute("alarms", new ArrayList<Alarm>(alarms));
 		req.getSession().setAttribute("usuario", usuario);
 		
 		RequestDispatcher view = req.getRequestDispatcher("Info.jsp");
